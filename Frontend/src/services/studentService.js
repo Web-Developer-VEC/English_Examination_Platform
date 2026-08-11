2cb988480e3f190c81a1d4aaa17d4d71995e5ce0
Jasim-Saad-P-A <jasim860p@gmail.com>
2026-08-11 13:35:50 +0530

    Intergration

--- Frontend/src/services/studentService.js
+++ Frontend/src/services/studentService.js
@@ -1,1 +1,15 @@
-export const verifyTestCode = async () => {};
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/staff/questions";
+
+export const startExam = async (testcode, admissionNo) => {
+    const response = await axios.post(
+        `${API_URL}/startexam`,
+        {
+            testcode,
+            admissionNo,
+        }
+    );
+
+    return response.data;
+};
