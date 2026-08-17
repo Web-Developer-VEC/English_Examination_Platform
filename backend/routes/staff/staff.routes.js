fb287d89fb2ce818f2b62eb7b1fe4483953ca0f0
Nithy1102 <anantham1102@gmail.com>
2026-08-17 15:21:31 +0530

    Update staff.routes.js

--- backend/routes/staff/staff.routes.js
+++ backend/routes/staff/staff.routes.js
@@ -6,11 +6,13 @@ const staffAuth = require("../../middleware/roleby.access.middleware");
 const questions_upload_Middleware = require("../../middleware/questionupload_middleware");
 const { questionsupload } = require("../../controllers/questions.controller");
 const { scheduleExam } = require("../../controllers/schedule.controller");
+const {getStudentsByDepartmentAndBatch}=require("../../controllers/getscheduleexam.controller");
 
 const student_upload_Middleware = require("../../middleware/student_upload_middleware");
 const { studentsUpload } = require("../../controllers/student.controller");
 const { updateStudent } = require("../../controllers/student.controller");
 const scheduleRoutes = require("./schedule.routes");
+const { generateExamReport } = require("../../controllers/result.controller");
 
 // Upload Student Excel
 router.post("/studentsupload", student_upload_Middleware, studentsUpload);
@@ -18,7 +20,8 @@ router.put("/studentsupdate", student_upload_Middleware, updateStudent);
 // Upload Audio + Excel
 router.post("/questionsupload", questions_upload_Middleware, questionsupload);
 
-
+router.get("/exam-results", generateExamReport);
+router.get("/student-data",getStudentsByDepartmentAndBatch);
 
 router.use("/schedule", scheduleRoutes);
 
