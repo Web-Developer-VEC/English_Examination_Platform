a6a3e940c4175e8df9c7bf97dc80a6c6e6541a85
uhemandkumaar-debug <u.hemandkumaar@gmail.com>
2026-08-24 10:58:18 +0530

    update routes

--- backend/routes/staff/staff.routes.js
+++ backend/routes/staff/staff.routes.js
@@ -14,6 +14,7 @@ const { updateStudent } = require("../../controllers/student.controller");
 const scheduleRoutes = require("./schedule.routes");
 const { generateExamReport } = require("../../controllers/result.controller");
 const{deleteQuestionSet}=require("../../controllers/delete.question.controller");
+const { getStaff, updateStaff } = require("../../controllers/staff.controller");
 
 // Upload Student Excel
 router.post("/studentsupload", student_upload_Middleware, studentsUpload);
