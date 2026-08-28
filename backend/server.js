675d684b2c05481a1bc118ea7848e1cbc96d3b4b
uhemandkumaar-debug <u.hemandkumaar@gmail.com>
2026-08-28 14:07:46 +0530

    security

--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,106 @@
 const cors = require("cors");
 const express = require("express");
 require("dotenv").config();
+
 const { connectDB } = require("./config/db");
+
+const secureNoSQLMiddleware =
+    require("./middleware/security/nosql_injection");
+
+const scheduleMongoHealthCheck =
+    require("./middleware/security/mongo_health_check");
+const xssSanitizer = require("./middleware/security/xss");
+
 const indexRoutes = require("./routes/index.routes");
-const startExamCron = require("./middleware/cron_middleware");
+const startExamCron =
+    require("./middleware/cron_middleware");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
+    cors({
+        origin: "http://localhost:5173",
+    }),
 );
-app.use(express.json());
 
-// Connect to MongoDB
-connectDB();
-
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connect to MongoDB
+    connectDB().then(() => {
+        console.log("✅ MongoDB connected successfully.");
+
+        // Start MongoDB health check only after DB connection
+        scheduleMongoHealthCheck();
+    })
+    .catch((error) => {
+        console.error("❌ MongoDB Connection Failed:");
+        console.error(error.message);
+    });
+
+
+
+app.use(xssSanitizer);
+// NoSQL Injection Protection
+app.use(secureNoSQLMiddleware);
+
+// Request Logger
 app.use((req, res, next) => {
-  const start = Date.now();
 
-  res.on("finish", () => {
-    console.log("Hits :",req.originalUrl);
-  });
+    const start = Date.now();
 
-  next();
+    res.on("finish", () => {
+
+        console.log(
+            "Hits:",
+            req.originalUrl,
+            "Status:",
+            res.statusCode,
+            "Time:",
+            Date.now() - start,
+            "ms"
+        );
+
+    });
+
+    next();
 });
+
 // Register Routes
 app.use("/api", indexRoutes);
 
 // 404 Handler
 app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-  });
+
+    res.status(404).json({
+        success: false,
+        message: "Route not found",
+    });
+
 });
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-  console.error(err);
 
-  res.status(err.status || 500).json({
-    success: false,
-    message: err.message || "Internal Server Error",
-  });
+    console.error(err);
+
+    res.status(err.status || 500).json({
+        success: false,
+        message:
+            err.message ||
+            "Internal Server Error",
+    });
+
 });
 
 // Start Server
 app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
-  startExamCron();
+
+    console.log(
+        `🚀 Server running on http://localhost:${port}`
+    );
+
+    startExamCron();
+
 });
