710dfb6f691a2697d9472c94049ec052a62b356e
Nithy1102 <anantham1102@gmail.com>
2026-08-31 10:43:02 +0530

    result completed

--- backend/controllers/result.controller.js
+++ backend/controllers/result.controller.js
@@ -5,7 +5,7 @@ const puppeteer = require("puppeteer");
 const { ObjectId } = require("mongodb");
 
 const { getDB } = require("../config/db");
-const { getFromS3, uploadToS3 } = require("../service/s3.service");
+const { uploadToS3 } = require("../service/s3.service");
 
 // ============================================================
 // GENERATE EXAM REPORT PDF
@@ -385,16 +385,16 @@ const generateExamReport = async (req, res) => {
     }
 
     // ====================================================
-    // GET LOGO FROM S3
+    // GET LOGO FROM LOCAL FILE
     // ====================================================
-    const logoKey = "english_exam_platform/assets/logo.png";
+    const logoPath = path.join(__dirname, "../assets/vec-logo.png");
     let logoBase64;
 
     try {
-      const logoBuffer = await getFromS3(logoKey);
+      const logoBuffer = fs.readFileSync(logoPath);
       logoBase64 = logoBuffer.toString("base64");
     } catch (error) {
-      console.error("S3 Logo Error:", error);
+      console.error("Local Logo Error:", error);
       return res.status(500).json({
         success: false,
         message: "College logo not found.",
