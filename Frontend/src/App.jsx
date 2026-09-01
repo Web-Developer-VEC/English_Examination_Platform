59ebb44a8573bd8767ce22223e6a5749582afbb0
Jasim-Saad-P-A <jasim860p@gmail.com>
2026-09-01 13:46:54 +0530

    Phone protection

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import Header from "./components/common/header";
 import AppRoutes from "./routes/AppRoutes";
+import DesktopOnly from "./components/common/DesktopOnly";
 
 function App() {
     return (
-        <div className="min-h-screen bg-white">
-            <div className="min-h-screen bg-white/40 flex flex-col">
+        <DesktopOnly>
+            <div className="min-h-screen bg-white">
+                <div className="min-h-screen bg-white/40 flex flex-col">
 
-                <Header />
+                    <Header />
 
-                <main className="flex-1 pt-[160px]">
-                    <AppRoutes />
-                </main>
+                    <main className="flex-1 pt-[160px]">
+                        <AppRoutes />
+                    </main>
 
+                </div>
             </div>
-        </div>
+        </DesktopOnly>
     );
 }
 
